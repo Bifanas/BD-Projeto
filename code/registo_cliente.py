229192cd7c2de3fc7c0f2b8cde7bb62e407c71b0
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-21 12:49:18 +0000

    addeed registo

--- code/registo_cliente.py
+++ code/registo_cliente.py
@@ -5,11 +5,19 @@ cur = conn.cursor()
  # REGISTO DO CLIENTE
  # Os ciclos while servem para fazer a verificacao da variavel
 
-def registo():
+def registo_cliente():
 
     us = input('Username: ')
     while(len(us) < 8):
         us = input('Insira o Username com pelo menos 8 digitos: ')
+
+    cur.execute("SELECT count(username) FROM cliente WHERE username = %s;",(us,))
+    a = cur.fetchone()[0]
+    while (a != 0):
+        us = input('Insira outro Username: ')
+        cur.execute("SELECT count(username) FROM cliente WHERE username = %s;", (us,))
+        a = cur.fetchone()[0]
+
 
     senha = input('Senha: ')
     while(len(senha) < 8):
@@ -28,26 +36,51 @@ def registo():
         email = input('Insira o Email com pelo menos 8 digitos: ')
 
     dia = input('Dia de nascimento: ')
-    d=eval(dia)
+    if(dia[0] == '0'):
+        d=eval(dia[1])
+    else:
+        d=eval(dia)
     while(d<1 or d>31):
         dia = input('Dia de nascimento: ')
         d = eval(dia)
+        if(dia[0] == '0'):
+            d=eval(dia[1])
+        else:
+            d=eval(dia)
 
     mes = input('Mes de nascimento: ')
-    m = eval(mes)
+    if(mes[0] == '0'):
+        m = eval(mes[1])
+    else:
+        m = eval(mes)
     while (m < 1 or m > 12):
         mes = input('Mes de nascimento: ')
         m = eval(mes)
+        if(dia[0] == '0'):
+            d=eval(dia[1])
+        else:
+            d=eval(dia)
 
     ano = input('Ano de nascimento: ')
-    a = eval(ano)
+    if(ano[0] == 0):
+        a = eval(ano[1])
+    else:
+        a = eval(ano)
+
     while (a < 1900 ):
         ano = input('Ano de nascimento: ')
         a = eval(ano)
+        if (ano[0] == 0):
+            a = eval(ano[1])
+        else:
+            a = eval(ano)
+
 
     data = dia + '-' + mes + '-' + ano
     cur.execute("INSERT INTO cliente values (%s,%s,%s,%s,%s,%s,20)", (us, senha, nome, email, endereco, data))
     conn.commit()
+
+registo_cliente()
 
 cur.close()
 conn.close()
