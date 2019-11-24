afd8fd15ddd4311a98b5ce3b7890843ddbc10157
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-24 17:21:26 +0000

    nao está completo

--- code/adicionar_album.py
+++ code/adicionar_album.py
@@ -8,69 +8,37 @@ cur = conn.cursor()
 # ADICIONAR ALBUM À BASE DE DADOS
 
 def adicionar_album():
-
-    # Pede que digite o albumID do album e verifica se este ja existe na base de dados
-    albumID = eval(input("AlbumID: "))
-    cur.execute("SELECT count(albumid) FROM album WHERE albumid = %s;", (albumID,))
-    cont = cur.fetchone()[0]
-
-    while (cont != 0):
-        albumID = eval(input('Insira outro albumID: '))
+    a=1
+    while a:
+        # Pede que digite o albumID do album e verifica se este ja existe na base de dados
+        albumID = eval(input("AlbumID: "))
         cur.execute("SELECT count(albumid) FROM album WHERE albumid = %s;", (albumID,))
         cont = cur.fetchone()[0]
 
-    a = 1
-    while a:
-        Nome = input("Nome do Album: ")
-        if(len(Nome) != None ):
-            a=0
+        while (cont != 0):
+            albumID = eval(input('Insira outro albumID: '))
+            cur.execute("SELECT count(albumid) FROM album WHERE albumid = %s;", (albumID,))
+            cont = cur.fetchone()[0]
 
-    a = 1
-    while a:
-        Tempo = input("Duracao do Album: ")
-        if(len(Tempo) != None ):
-            a=0
+        a = 1
+        while a:
+            Nome = input("Nome do Album: ")
+            if (len(Nome) != None):
+                a = 0
 
-    print("Data de Lançamento:")
-    data = funcaoData.data()
+        a = 1
+        while a:
+            Tempo = input("Duracao do Album: ")
+            if (len(Tempo) != None):
+                a = 0
 
-    Stock = eval(input("Quantidade em stock: "))
+        print("Data de Lançamento:")
+        data = funcaoData.data()
 
-    Preco = eval(input("Preco: "))
+        Stock = eval(input("Quantidade em stock: "))
 
-    cur.execute("INSERT INTO album values (%s,%s,%s,%s,%s,%s,0)", (albumID, Nome, Tempo, data, Stock, Preco))
-    conn.commit()
+        Preco = eval(input("Preco: "))
 
-    a = 1
-    while a:
-        ID = add.adicionar_musica()
-        cur.execute("INSERT INTO musica_album values (%s,%s)", (ID, albumID))
+        cur.execute("INSERT INTO album values (%s,%s,%s,%s,%s,%s,0)", (albumID, Nome, Tempo, data, Stock, Preco))
         conn.commit()
 
-        a = eval(input("Digite 1 para adicionar outra musica:"))
-        if(a != 1):
-            a = 0
-
-    a = 1
-    while a:
-        ID = add.adicionar_artista()
-        cur.execute("INSERT INTO artista_album values (%s,%s)", (ID, albumID))
-        conn.commit()
-
-        a = eval(input("Digite 1 para adicionar outro artista:"))
-        if(a != 1):
-            a = 0
-
-    a = 1
-    while a:
-        ID = add.adicionar_genero()
-        cur.execute("INSERT INTO genero_album values (%s,%s)", (ID, albumID))
-        conn.commit()
-
-        a = eval(input("Digite 1 para adicionar outro genero:"))
-        if(a != 1):
-            a = 0
-
-
-
-adicionar_album()
