b6288f3463d931fc2a6f311e6ac506750ac97647
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-25 00:55:05 +0000

    falta albumid, como implementar.

--- code/add_MGA.py
+++ code/add_MGA.py
@@ -2,6 +2,7 @@ import psycopg2
 conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
 cur = conn.cursor()
 
+
 # ADICIONA MUSICAS AO ALBUM
 def adicionar_musica():
     a = 1
