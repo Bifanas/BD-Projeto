86589f3e50dd24ffd50a61130e8116a6b2bd0aea
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-25 20:50:49 +0000

    Dados pro pgAdmin4

---
+++ code/dados_tabela_TESTE.py
@@ -0,0 +1,22 @@
+SELECT * FROM administrador
+
+INSERT INTO administrador
+VALUES (1, 'senha1', 'adm1', 987654321,'adm1@email.com')
+
+INSERT INTO administrador
+VALUES (2, 'senha2', 'adm3', 999666333,'adm2@email.com')
+
+INSERT INTO administrador
+VALUES (3, 'senha3', 'adm3', 987456321,'adm3@email.com')
+
+
+INSERT INTO cliente
+VALUES (1, 'senha1', 'cliente1', 'c1@email.com',20)
+
+INSERT INTO cliente
+VALUES (2, 'senha2', 'cliente2', 'c2@email.com',20)
+
+INSERT INTO cliente
+VALUES (3, 'senha3', 'cliente3', 'c3@email.com',20)
+
+
