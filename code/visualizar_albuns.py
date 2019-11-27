f1fc5bc3317d4a691e97c89c35897d482aa9dc63
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-27 12:51:06 +0000

    Completo! :)

--- code/visualizar_albuns.py
+++ code/visualizar_albuns.py
@@ -1,22 +1,17 @@
-import psycopg2
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
-cur = conn.cursor()
+# A funcao imprime serve para dar um espaco entre cada informacao sobre o album
+# A funcao fucn mostra todos os albuns disponiveis em stock
 
-# Mostra todos os albuns disponiveis em stock
 def imprime(linha):
     print('       '.join(map(str, linha)))
 
 
-def func():
+def func(cur):
     a = 1
     while a:
         cur.execute("SELECT * FROM album WHERE stock > 0;")
-        print ("ID | NOME | DURACAO | DATA  | QUANT | STOCK")
+        print ("ID       NOME       DURACAO     DATA      STOCK    PRECO")
 
         for linha in cur.fetchall():
             imprime(linha)
 
         a = eval(input("Insere 0 para voltar: "))
-
-
-func()
