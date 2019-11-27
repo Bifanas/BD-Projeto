59c9a31e6bafea3b656ee3e6c54de0059e493ba5
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-27 12:35:55 +0000

    Erro ao relacionar as duas tabelas

--- code/add_MGA.py
+++ code/add_MGA.py
@@ -1,36 +1,60 @@
 import psycopg2
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
-cur = conn.cursor()
 
-# ADICIONA MUSICAS AO ALBUM
-def adicionar_musica():
+# ADICIONA MUSICAS
+def adicionar_musica(conn,cur):
     a = 1
     while a != 0:
         Nome = input("Nome da Musica: ")
-        Tempo = input("Duracao da musica: ")
+        cur.execute("INSERT INTO musica (musica) values (%s)", (Nome, ))
+        conn.commit()
 
-        cur.execute("INSERT INTO musica values (%s,%s)", (Nome, Tempo))
+        cur.execute("SELECT MAX(id) FROM album")
+        id_a = cur.fetchone()[0]
+
+        cur.execute("SELECT MAX(id) FROM musica")
+        id_m = cur.fetchone()[0]
+
+        cur.execute("INSERT INTO musica_album values (%s%s)", (id_m,id_a))
         conn.commit()
 
         a = eval(input("Insere 0 para voltar: "))
 
 
 # ADICIONA UM GENERO
-def adicionar_genero():
+def adicionar_genero(conn,cur):
     a = 1
     while a != 0:
         tipo_genero = input("Nome do Genero: ")
-        cur.execute("INSERT INTO genero values (%s)", (tipo_genero))
+        cur.execute("INSERT INTO genero (tipo_genero) values (%s)", (tipo_genero))
         conn.commit()
 
+        cur.execute("SELECT MAX(id) FROM album")
+        id_a = cur.fetchone()[0]
 
-# ADICIONA UM ARTISTA
-def adicionar_artista():
+        cur.execute("SELECT MAX(id) FROM genero")
+        id_g = cur.fetchone()[0]
+
+        cur.execute("INSERT INTO album_genero values (%s%s)", (id_a, id_g))
+        conn.commit()
+
+        a = eval(input("Insere 0 para voltar: "))
+
+
+# ADICIONA ARTISTAs
+def adicionar_artista(conn, cur):
     a = 1
     while a != 0:
         Nome = input("Nome do Artista: ")
 
-        cur.execute("INSERT INTO artista values (%s)", (Nome,))
+        cur.execute("INSERT INTO artista (artista) values (%s)", (Nome,))
+        conn.commit()
+
+        cur.execute("SELECT MAX(id) FROM album")
+        id_a = cur.fetchone()[0]
+
+        cur.execute("SELECT MAX(id) FROM artista")
+        id_art = cur.fetchone()[0]
+        cur.execute("INSERT INTO artista_album values (%s%s)", (id_art, id_a))
         conn.commit()
 
         a = eval(input("Insere 0 para voltar: "))
