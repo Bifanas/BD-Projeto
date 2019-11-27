cfe341c1c1a378af3b2b735aa427a0ef9edd5e51
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-27 12:37:03 +0000

    Completo, porem precisa da funcao add_MGA

--- code/adicionar_album.py
+++ code/adicionar_album.py
@@ -1,32 +1,27 @@
 import psycopg2
-import funcaoData
 import add_MGA
-
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
-cur = conn.cursor()
-
+import datetime
 # ADICIONAR ALBUM À BASE DE DADOS
 # Verifica se o nome e o tempo nao sao nulos
 # É Pedido a data de lancamento, stock atual e o preco do album
 
 
-def func():
+def func(conn, cur):
     a=1
-    while a:
+    while a!=0:
 
         nome = input("Digite o nome do novo album: ")
-        Tempo = input("Duracao do Album: ")
-
-        print("Data de Lançamento:")
-        data = funcaoData.data()
+        duracao = input("Duracao do Album: ")
+        ano = eval(input("Ano: "))
         Stock = eval(input("Quantidade em stock: "))
         Preco = eval(input("Preco: "))
 
-        cur.execute("INSERT INTO album values (6,%s,%s,%s,%s,%s)", (nome, Tempo, data, Stock, Preco))
-        cur.execute("INSERT INTO historico_a values (%s,%s,6)", (Preco, Stock))
+        cur.execute("INSERT INTO album (nome, duracao, ano, stock, preco) values (%s,%s,%s,%s,%s)", (nome, duracao, ano, Stock, Preco))
         conn.commit()
 
-        add_MGA.adicionar_musica()
-        add_MGA.adicionar_genero()
-        add_MGA.adicionar_artista()
+        add_MGA.adicionar_musica(conn, cur, )
+        add_MGA.adicionar_artista(conn, cur)
+        add_MGA.adicionar_genero(conn, cur)
 
+        a = eval(input("Insere 0 para voltar: "))
+        print("----------------------------------------------------------------------------------")
