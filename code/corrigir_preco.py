acbed9cf93bad45c393b8447f1ad4f84830b4e4e
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-28 10:35:13 +0000

    COMPLETO! :)

--- code/corrigir_preco.py
+++ code/corrigir_preco.py
@@ -8,35 +8,38 @@ import datetime
 def imprime(linha):
     print('       '.join(map(str, linha)))
 
-def func(conn, cur):
+def func(conn, cur,adm):
     a = 1
+    b = 1
+
+    #Enquanto o adm nao inserir 0 para voltar ao menu ficará removendo album
     while a != 0:
-        q=0
-        nome = input("Insira o nome do album: ")
-        cur.execute("SELECT count(*) FROM album WHERE nome = %s GROUP BY nome;", (nome,))
-        q = cur.fetchone()[0]
-        if (q == 0):
-            print("Não existe album com este nome.")
 
-        elif(q > 1):
-            print("ID       NOME       PRECO")
-            cur.execute("SELECT id, nome, preco FROM album WHERE nome = %s ORDER BY id;", (nome,))
-            for linha in cur.fetchall():
-                imprime(linha)
-            id = eval(input("Insira o albumID: "))
+        #Enquanto o adm nao inserir o nome correto ficara pedindo o nome do album
+        while b != 0:
+            q = 0
+            nome = input("Insira o nome do album: ")
+            cur.execute("SELECT count(*) FROM album WHERE nome = %s GROUP BY nome;", (nome,))
+            q = cur.fetchone()
 
-        else:
-            cur.execute("SELECT id FROM album WHERE nome = %s;", (nome,))
-            id = cur.fetchone()[0]
+            if (q is None):
+                print("Não existe album com este nome.")
+                b=1
+
+            else:
+                print("ID       NOME       PRECO")
+                cur.execute("SELECT id, nome, preco FROM album WHERE nome = %s ORDER BY id;", (nome,))
+                for linha in cur.fetchall():
+                    imprime(linha)
+                id = eval(input("Insira o albumID: "))
+                b=0
 
         preco = eval(input("Insira o novo preco: "))
-        cur.execute("SELECT preco FROM album WHERE id = %s;", (id,))
-        p_ant = cur.fetchone()[0]
 
         now = datetime.datetime.now()
         data = (str(now.day) + '-' + str(now.month) + '-' + str(now.year) + ' ' + str(now.hour) + ':' + str(now.minute))
 
-        cur.execute("INSERT INTO historico_a values (%s,%s,1,%s)", (p_ant, data, id))
+        cur.execute("INSERT INTO historico_a values (%s,%s,%s,%s)", (preco, data, adm, id))
 
 
         cur.execute("UPDATE album SET preco = %s WHERE id = %s;", (preco, id))
