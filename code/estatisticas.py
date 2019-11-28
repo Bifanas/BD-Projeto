80b7d2ed43b0f3d0f5a114d43a294ba4f0db8f11
Naiara Simões <naiaraasimoes@gmail.com>
2019-11-28 17:42:46 +0000

    Incompleto, falta genero musical e algum outra

--- code/estatisticas.py
+++ code/estatisticas.py
@@ -1,34 +1,40 @@
-import psycopg2
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
-cur = conn.cursor()
+def imprime(linha):
+    print('    '.join(map(str, linha)))
 
-def func():
-    a = 1
-    while a:
+def func(cur):
+    a = '1'
+    while a != '0':
         cur.execute("SELECT count(username) FROM cliente;")
-        cont = cur.fetchone()[0]
-        print("TOTAL DE CLIENTES: ", cont)
+        a = cur.fetchone()[0]
+        print("\nTOTAL DE CLIENTES: ", a)
 
-        cur.execute("SELECT count(albumid) FROM album;")
-        cont = cur.fetchone()[0]
-        print("\nTOTAL DE DISCOS: ", cont)
+        cur.execute("SELECT count(id) FROM album;")
+        b = cur.fetchone()[0]
+        print("TOTAL DE DISCOS: ", b)
 
-        cur.execute("SELECT count(preco) FROM album WHERE stock >0;")
-        cont = cur.fetchone()[0]
-        print("\nVALOR TOTAL DOS DISCOS EM STOCK: ", cont)
+        cur.execute("SELECT SUM(preco) FROM album WHERE  stock > 0;")
+        c = cur.fetchone()[0]
+        print("VALOR TOTAL DOS DISCOS EM STOCK: ", c)
 
-        cur.execute("SELECT count(preco) FROM historico_c;")
-        cont = cur.fetchone()[0]
-        print("\nVALOR TOTAL DAS VENDAS: ", cont)
+        cur.execute("SELECT SUM (preco) FROM historico_c_album, album WHERE album.id = historico_c_album.album_id;")
+        d = cur.fetchone()[0]
+        print("VALOR TOTAL DAS VENDAS: ", d)
 
-        cur.execute("SELECT DISTINCT genero_tipo_genero, count(*) FROM album_genero GROUP BY genero_tipo_genero;")
-        print("\nTOTAL DE DISCOS POR GENERO MUSICAL: ")
+        cur.execute("SELECT count(id) FROM artista;")
+        e = cur.fetchone()[0]
+        print("TOTAL DE ARTISTAS: ", e)
+
+        cur.execute("SELECT nome FROM  historico_c_album, album WHERE album.id = historico_c_album.album_id GROUP BY nome ORDER BY count(nome) DESC;")
+        print("ALBUNS MAIS VENDIDOS: ")
         for linha in cur.fetchall():
-            print(linha)
+            imprime(linha)
 
-        cur.execute("SELECT DISTINCT artista, count(*) FROM artista_genero  GROUP BY ;")
-        print("\nTOTAL DE DISCOS VENDIDOS POR ARTISTA: ")
+
+        cur.execute("SELECT tipo_genero, stock FROM genero, album, album_genero WHERE album.id = album_genero.album_id and genero.id = album_genero.genero_id ORDER BY stock DESC;")
+        print("TOTAL DE DISCOS POR GENERO MUSICAL: ")
         for linha in cur.fetchall():
-            print(linha)
+            imprime(linha)
 
-        a = eval(input("Insere 0 para voltar: "))
+
+
+        a = input("\nInsere 0 para voltar: ")
