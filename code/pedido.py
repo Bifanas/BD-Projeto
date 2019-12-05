616b121e30d1ec073cc6639b3892f40fdca73d23
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-05 03:40:02 +0000

    COMPLETO!

---
+++ code/pedido.py
@@ -0,0 +1,48 @@
+import operacoes_carrinho
+
+def imprime(linha):
+    print(' - '.join(map(str, linha)))
+
+def func(conn,cur, id):
+    cur.execute("SELECT count(album_id) FROM pedido ")
+    q = cur.fetchone()[0]
+
+    x=0
+    if (q == 0):
+        print("Carrinho está vazio.")
+
+    else:
+        cur.execute("SELECT album_id, nome, preco FROM pedido, album WHERE cliente_id = %s and album_id = album.id;",
+                    (id,))
+        for linha in cur.fetchall():
+            imprime(linha)
+
+    x = 0
+    while x != '4':
+        print('\n1 - Adicionar album\n2 - Remover album\n3 - Finalizar compras\n4 - Logout')
+        x = input('\n')
+
+        if x == '1':
+            print('Adicionar album')
+            operacoes_carrinho.add(conn, cur, id)
+
+        elif x == '2':
+            print('Remover album')
+            if(q == 0):
+                 print("Nao é possivel remover.")
+            else:
+                operacoes_carrinho.rem(conn, cur, id)
+
+        elif x == '3':
+            print('Finalizar compras')
+            if (q == 0):
+                print("Nao é possivel finalizar compras.")
+            else:
+                operacoes_carrinho.finalizar(conn, cur, id)
+
+        elif x == '4':
+            print('Logout')
+
+        else:
+            print("Opcao nao valida")
+
