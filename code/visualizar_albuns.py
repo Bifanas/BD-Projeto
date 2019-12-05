900f8fd9e38f509648054929d0fe5f17c07be3e1
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-05 00:14:11 +0000

    COMPLETO! (perguntar ao prof)

--- code/visualizar_albuns.py
+++ code/visualizar_albuns.py
@@ -2,12 +2,12 @@
 # A funcao fucn mostra todos os albuns disponiveis em stock
 
 def imprime(linha):
-    print('       '.join(map(str, linha)))
+    print('   '.join(map(str, linha)))
 
 def func(cur):
     a = '1'
     cur.execute("SELECT * FROM album WHERE stock > 0 ORDER BY ID;")
-    print ("ID       NOME       DURACAO     DATA      STOCK    PRECO")
+    print ("ID-NOME-DURACAO-DATA-STOCK-PRECO")
     for linha in cur.fetchall():
         imprime(linha)
     while a != '0':
