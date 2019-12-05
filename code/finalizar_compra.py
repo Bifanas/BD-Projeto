40b58e6d21c873bcc5bbf1805c6e20de31d1a4da
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-05 10:31:04 +0000

    COMPLETO!

--- code/finalizar_compra.py
+++ code/finalizar_compra.py
@@ -1,9 +1,4 @@
 import datetime
-import psycopg2
-# vai importar todas as funcoes dos arquivos
-
-conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=postgres")
-cur = conn.cursor()
 
 def finalizar(conn, cur, id):
     # Regista o saldo do cliente disponivel
@@ -21,45 +16,63 @@ def finalizar(conn, cur, id):
         print("Dinheiro insuficiente.")
 
     else:
+        #Procura ultimo id
+        cur.execute("SELECT MAX(id) FROM historico_c WHERE cliente_id = %s;", (id,))
+        j = cur.fetchone()[0]
+
+        if(j is None):
+            j=1
+        else:
+            j+=1
+
+        #Regista novo pedido
+        now = datetime.datetime.now()
+        data = (str(now.day) + '-' + str(now.month) + '-' + str(now.year) + ' ' + str(now.hour) + ':' + str(now.minute) + ':' + str(now.second))
+        cur.execute("INSERT INTO historico_c  VALUES (%s,%s,%s);", (j, data, id))
+        conn.commit()
+
+
         # Regista quantos albuns estao no pedido
         cur.execute("SELECT count(album_id) FROM pedido WHERE cliente_id = %s;", (id,))
         p = cur.fetchone()[0]
 
-        now = datetime.datetime.now()
-        data = (str(now.day) + '-' + str(now.month) + '-' + str(now.year) + ' ' + str(now.hour) + ':' + str(now.minute) + ':' + str(now.second))
-        cur.execute("INSERT INTO historico_c (data_de_compra, cliente_id) VALUES (%s,%s);", (data, id))
-        conn.commit()
-
+        #Procura o maior id registado em historico__C
         cur.execute("SELECT MAX(id) FROM historico_c WHERE cliente_id = %s;", (id,))
         j = cur.fetchone()[0]
 
         # Enquanto tiver pedido no carrinho
         while p != 0:
-
+            #Procura o menor id dos albuns que tem no carrinho
             cur.execute("SELECT MIN(album_id) FROM pedido WHERE cliente_id = %s;", (id,))
             i = cur.fetchone()[0]
 
+            # Ve quantos tem em stock e substrai 1
+            cur.execute("SELECT stock FROM album WHERE id = %s;", (i,))
+            stock = cur.fetchone()[0]
+
+            # Insere no historico_c
             cur.execute("INSERT INTO historico_c_album VALUES (%s,%s);", (j, i))
             conn.commit()
 
-            cur.execute("SELECT stock FROM album WHERE id = %s;", (i,))
-            stock = cur.fetchone()[0]
             stock -= 1
-
-            cur.execute("UPDATE album SET stock = %s WHERE id = %s;",(stock, i ))
+            cur.execute("UPDATE album SET stock = %s WHERE id = %s;", (stock, i))
             conn.commit()
 
-            cur.execute("SELECT preco FROM album WHERE id = %s;",(i, ))
+            # Precos do album atual
+            cur.execute("SELECT preco FROM album WHERE id = %s;", (i,))
             preco = cur.fetchone()[0]
 
-            s_atual = saldo - preco
-
-            cur.execute("UPDATE cliente SET saldo = %s WHERE id = %s;", (s_atual,id))
+            # Atualizacao do saldo do cliente
+            s_atual = (saldo-preco)
+            cur.execute("UPDATE cliente SET saldo = %s WHERE id = %s;", (s_atual, id))
             conn.commit()
 
+            #Deleta album comprado do carrinho
             cur.execute("DELETE FROM pedido WHERE album_id = %s and cliente_id = %s;", (i, id,))
             conn.commit()
 
+            #Conta quantos albuns tem no carrinho
+            cur.execute("SELECT count(album_id) FROM pedido WHERE cliente_id = %s;", (id,))
+            p = cur.fetchone()[0]
 
-
-finalizar(conn,cur,1)
+        print("Albuns comprados.")
