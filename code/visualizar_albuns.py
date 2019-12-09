8cd98aafb763a3750ade5e3a306fb434dd9680f3
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-09 01:01:09 +0000

    Verificado.

--- code/visualizar_albuns.py
+++ code/visualizar_albuns.py
@@ -5,13 +5,17 @@ def imprime(linha):
     print('   '.join(map(str, linha)))
 
 def func(cur):
+    print('\nVisualiza albuns:')
     a = '1'
     #Procura todos albuns disponiveis em stock e mostra no ecra
     cur.execute("SELECT * FROM album WHERE stock > 0 ORDER BY ID;")
-    print ("ID-NOME-DURACAO-DATA-STOCK-PRECO")
+    print ("ID  Nome Duração Data Stock Preço")
     for linha in cur.fetchall():
         imprime(linha)
+    print(" ")
 
     #Espera pelo utilizador primir 0 para voltar ao menu
     while a != '0':
         a =input("Insere 0 para voltar: ")
+
+    print(" ")
