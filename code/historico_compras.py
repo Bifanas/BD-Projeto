80b1df21f11edcad8d104253312557633d73fb16
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-09 00:15:13 +0000

    Verificado.

--- code/historico_compras.py
+++ code/historico_compras.py
@@ -1,31 +1,41 @@
 import ordena_historico
 
-def func(conn, cur,id):
+def func(cur,id):
     x = '0'
     while x != '4':
-        print('1 - Ordenar por Album\n2 - Ordenar por Musica\n3 - Ordenar por Genero Musical\n4 - Ordenar por Grupo\n5 - Retornar')
-        # Mostra as opcoes
+        print('\n1 - Ordenar por Album\n2 - Ordenar por Musica\n3 - Ordenar por Genero Musical\n4 - Ordenar por Grupo\n5 - Pedidos Anteriores\n6 - Valor Gasto por Genero Musical\n7 - Retornar')
         x = input('\n')
 
+        cur.execute("SELECT nome FROM cliente WHERE id = %s;", (id,))
+        nome = cur.fetchone()[0]
+
         if x == '1':
-            print('Ordenar por album')
+            print('\nUsuario:', nome)
             ordena_historico.por_album(cur, id)
 
 
         elif x == '2':
-            print('Ordenar por musica')
+            print('\nUsuario:', nome)
             ordena_historico.por_musica(cur, id)
 
         elif x == '3':
-            print('Ordenar por genero')
+            print('\nUsuario:', nome)
             ordena_historico.por_genero(cur, id)
 
         elif x == '4':
-            print('Ordenar por grupo')
+            print('\nUsuario:', nome)
             ordena_historico.por_grupo(cur, id)
 
         elif x == '5':
-            print('Retornar')
+            print('\nUsuario:', nome)
+            ordena_historico.pedidos_anteriores(cur, id)
+
+        elif x == '6':
+            print('\nUsuario:', nome)
+            ordena_historico.distincao(cur,id)
+
+        elif x == '7':
+            print('\nRetornar')
 
         else:
             print("\nOpcao nao valida")
