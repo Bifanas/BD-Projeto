b142f3b9cab1ab5754f1eb52ae3930d0a7aad9f2
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-09 22:04:57 +0000

    PERFETTO

--- code/visualizar_albuns.py
+++ code/visualizar_albuns.py
@@ -1,21 +1,24 @@
-# A funcao imprime serve para dar um espaco entre cada informacao sobre o album
-# A funcao fucn mostra todos os albuns disponiveis em stock
-
-def imprime(linha):
-    print('   '.join(map(str, linha)))
 
 def func(cur):
-    print('\nVisualiza albuns:')
-    a = '1'
-    #Procura todos albuns disponiveis em stock e mostra no ecra
-    cur.execute("SELECT * FROM album WHERE stock > 0 ORDER BY ID;")
-    print ("ID  Nome Duração Data Stock Preço")
-    for linha in cur.fetchall():
-        imprime(linha)
-    print(" ")
+    #Conta quantos albuns há registados
+    cur.execute("SELECT count(*) FROM album WHERE stock > 0;")
+    q = cur.fetchone()[0]
 
-    #Espera pelo utilizador primir 0 para voltar ao menu
+    if(q == 0):
+        print("\n")
+        print("Não há albuns registados.")
+
+    else:
+        # Procura todos albuns disponiveis em stock e mostra no ecra
+        print('\nVisualiza albuns:')
+        cur.execute("SELECT * FROM album WHERE stock > 0 ORDER BY ID;")
+        for linha in cur.fetchall():
+            print("ID:", linha[0], " | Nome:", linha[1], " | Duração: ", linha[2], " | Data:", linha[3], " | Stock: ", linha[4], " | Preço: ", linha[5])
+
+    a='1'
     while a != '0':
-        a =input("Insere 0 para voltar: ")
+        a = input("Insere 0 para voltar: ")
 
     print("\n")
+
+
