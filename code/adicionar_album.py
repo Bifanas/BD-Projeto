ab6fb8ba01b543dae94ff4486b913a666af16c9a
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-09 15:11:57 +0000

    Verificado

--- code/adicionar_album.py
+++ code/adicionar_album.py
@@ -5,29 +5,26 @@ import add_artista
 # ADICIONAR ALBUM À BASE DE DADOS
 def func(conn, cur):
     print('\nAdicionar album')
-    a= '1'
-    while a!= '0':
-        # DADOS DO ALBUM PEDIDOS
-        nome = input("Digite o nome do novo album: ")
-        duracao = input("Duracao do Album: ")
-        Stock = eval(input("Quantidade em stock: "))
-        Preco = eval(input("Preco: "))
-        ano = eval(input("Ano: "))
 
-        # PROCURA O ULTIMO ID REGISTADO E ADICIONA O ALBUM NO PROXIMO ID
-        cur.execute("SELECT MAX(id) FROM album")
-        id_a = cur.fetchone()[0]
+    # DADOS DO ALBUM PEDIDOS
+    nome = input("Digite o nome do novo album: ")
+    duracao = input("Duracao do Album: ")
+    Stock = eval(input("Quantidade em stock: "))
+    Preco = eval(input("Preco: "))
+    ano = eval(input("Ano: "))
 
-        # VERIFICA SE NAO TEM NENHUM ALBUM REGISTADO
-        if (id_a is None):
-            id_a = 0
-        id_a += 1
-        cur.execute("INSERT INTO album values (%s,%s,%s,%s,%s,%s)", (id_a, nome, duracao, ano, Stock, Preco))
-        conn.commit()
+    # PROCURA O ULTIMO ID REGISTADO E ADICIONA O ALBUM NO PROXIMO ID
+    cur.execute("SELECT MAX(id) FROM album")
+    id_a = cur.fetchone()[0]
 
-        # FUNCAO QUE PERMITE ADICIONAR MUSICA, ARTISTA E GENERO
-        add_musica.adicionar_musica(conn, cur)
-        add_genero.adicionar_genero(conn, cur)
-        add_artista.adicionar_artista(conn, cur)
+    # VERIFICA SE NAO TEM NENHUM ALBUM REGISTADO
+    if (id_a is None):
+        id_a = 0
+    id_a += 1
+    cur.execute("INSERT INTO album values (%s,%s,%s,%s,%s,%s)", (id_a, nome, duracao, ano, Stock, Preco))
+    conn.commit()
 
-        a = input("Insere 0 para voltar ao menu: ")
+    # FUNCAO QUE PERMITE ADICIONAR MUSICA, ARTISTA E GENERO
+    add_musica.adicionar_musica(conn, cur)
+    add_genero.adicionar_genero(conn, cur)
+    add_artista.adicionar_artista(conn, cur)
