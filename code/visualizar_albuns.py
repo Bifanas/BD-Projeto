543de8f80d3f20b09cbab5aa711e50538ecb988f
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-09 22:10:28 +0000

    PERFETTO

--- code/visualizar_albuns.py
+++ code/visualizar_albuns.py
@@ -6,11 +6,11 @@ def func(cur):
 
     if(q == 0):
         print("\n")
-        print("Não há albuns registados.")
+        print("Não há álbuns registados.")
 
     else:
         # Procura todos albuns disponiveis em stock e mostra no ecra
-        print('\nVisualiza albuns:')
+        print('\nÁlbuns em Stock:')
         cur.execute("SELECT * FROM album WHERE stock > 0 ORDER BY ID;")
         for linha in cur.fetchall():
             print("ID:", linha[0], " | Nome:", linha[1], " | Duração: ", linha[2], " | Data:", linha[3], " | Stock: ", linha[4], " | Preço: ", linha[5])
