48dcc1fd73f7e738d792b05450ad0c98d431734a
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-09 00:07:09 +0000

    Verificado.

--- code/funcaoData.py
+++ code/funcaoData.py
@@ -11,33 +11,48 @@ def data():
     while(d<1 or d>31):
         dia = input('Dia: ')
         a = 0
-        if(dia[0] == '0'):
-            if(len(dia) != 2):
-                d=0
-            else:
-                if(dia[1] >= '1' and dia[1] <= '9' ):
-                    a = dia[1]
-                    d = eval(a)
+        #verifica se o usuario nao digitou nada diferente de numero
+        if(dia >= '' and dia <= '/' or dia >= ':' or dia == None ):
+            d=0
 
         else:
-            d = eval(dia)
+            if(dia[0] == '0'):
+                if(len(dia) != 2):
+                 d=0
+                else:
+                    if(dia[1] >= '1' and dia[1] <= '9' ):
+                        a = dia[1]
+                        d = eval(a)
+
+            else:
+                d = eval(dia)
 
     if(d<10 and a == 0):
         aux=dia
         dia = '0' + aux
 
+
     m = 0
     while (m < 1 or m > 12):
         mes = input('Mes: ')
         b = 0
-        if(mes[0] == '0'):
-            if(mes[1] >= '1' and mes <= '9'):
-                b = mes[1]
-                m = eval(b)
-            else:
-                m = 0
+
+        # verifica se o usuario nao digitou nada diferente de numero
+        if (mes >= '' and mes <= '/' or mes >= ':' or mes == None):
+            m = 0
+
         else:
-            m = eval(mes)
+            if(mes[0] == '0'):
+                if(len(mes) != 2):
+                    m=0
+
+                else:
+                    if(mes[1] >= '1' and mes <= '9'):
+                        b = mes[1]
+                        m = eval(b)
+
+            else:
+                m = eval(mes)
 
     if(m<10 and b == 0):
         aux = mes
@@ -54,3 +69,6 @@ def data():
 
     data = dia + '-'+ mes + '-' + ano
     return data
+
+
+data = data()
