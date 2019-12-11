9ac17b10368d44b55e5d766cfa17c79a75f1bb46
Naiara Simões <naiaraasimoes@gmail.com>
2019-12-11 17:50:50 +0000

    PERFETTO

--- code/pesquisar.py
+++ code/pesquisar.py
@@ -1,35 +1,32 @@
 import ordena_pesquisa
-def imprime(linha):\
-    print(' - '.join(map(str, linha)))
 
-def func(conn,cur,id):
+def func(conn,cur,id, nome):
     x = '0'
     while x != '5':
-        print("Ordenar por:")
+        print("\n")
+        print('Usuario:', nome)
+        print("ORDENAR POR:")
         print('1 - Album\n2 - Musica\n3 - Genero Musical\n4 - Grupo\n5 - Retornar')
         # Mostra as opcoes
         x = input('\n')
 
         if x == '1':
-            print('Ordenar por album')
-            ordena_pesquisa.por_album(conn,cur,id)
-
+            ordena_pesquisa.por_album(conn,cur,id, nome)
 
         elif x == '2':
-            print('Ordenar por musica')
-            ordena_pesquisa.por_musica(conn,cur,id)
+            ordena_pesquisa.por_musica(conn,cur,id, nome)
 
         elif x == '3':
-            print('Ordenar por genero')
-            ordena_pesquisa.por_genero(conn, cur, id)
+            ordena_pesquisa.por_genero(conn, cur, id, nome)
 
         elif x == '4':
-            print('Ordenar por grupo')
-            ordena_pesquisa.por_grupo(conn,cur,id)
+            ordena_pesquisa.por_grupo(conn,cur,id, nome)
 
         elif x == '5':
-            print('Retornar')
+            print('\n')
+            print("Voltar")
 
         else:
-            print("\nOpcao nao valida")
+            print('\n')
+            print("Opção não válida")
 
